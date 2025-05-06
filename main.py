b456381291d365789efc07bbbbc41993dbd8e744
aethrum <curiosidadespositivas@gmail.com>
2025-05-06 09:49:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -153,4 +153,4 @@ def ruta_no_encontrada(e):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)p
+    app.run(host="0.0.0.0", port=port)
